83bb182463d77afdded33568271ff932bc5e884b
geoffnelson94 <geoff.nelson116@gmail.com>
2022-03-22 15:14:56 -0500

    does it work w/o main

--- hello_test.cc
+++ hello_test.cc
@@ -8,8 +8,8 @@ TEST(HelloTest, BasicAssertions) {
   EXPECT_EQ(7 * 6, 42);
 }
 
-int main(int argc, char **argv) 
-{
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
+//int main(int argc, char **argv) 
+//{
+//  testing::InitGoogleTest(&argc, argv);
+//  return RUN_ALL_TESTS();
+//}
